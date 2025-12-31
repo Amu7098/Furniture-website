eb573aa9e3e711dbf6abfd633cffaf0e0e42147c
Aman Thakur <amanthakur.v18@gmail.com>
2025-12-31 12:52:57 +0530

    added add to cart functionality with dumy content

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,25 +1,43 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ProductContext } from "./context/ProductContext";
+import { FaPlusCircle } from "react-icons/fa";
+import { FaMinusCircle } from "react-icons/fa";
 
-function Product() {
-  const ProductLists = useContext(ProductContext);
+function item() {
+  const { productList, cartItems, addToCart, removeFromCart } =
+    useContext(ProductContext);
+  const acvCart = useState(false);
   return (
-   
     <div className="container-fluid">
       <div className="row">
-        {ProductLists.map((product) => (
-          <div className="col-md-3 mt-5" key={product.id}>
+        {productList.map((item) => (
+          <div className="col-md-3 mt-5" key={item.id}>
             <div className="card h-100">
-              <img
-                src={product.image}
-                className="product-img"
-                alt={product.name}
-              />
+              <img src={item.image} className="card-img-top" alt={item.name} />
 
               <div className="card-body">
-                <h6 className="card-title">{product.product}</h6>
-
-                <h6 className="text-success">₹{product.price}</h6>
+                <h6 className="card-title fw-bold">{item.product}</h6>
+                <p className="text-success fw-bold">₹{item.price}</p>
+                {!cartItems[item.id] ? (
+                  <button
+                    className="btn btn-dark"
+                    onClick={() => addToCart(item.id)}
+                  >
+                    Add to Cart
+                  </button>
+                ) : (
+                  <div>
+                    <FaMinusCircle
+                      className="text-danger fs-1 ms-3"
+                      onClick={() => removeFromCart(item.id)}
+                    />
+                    <span className="fw-bold mx-4">{cartItems[item.id]}</span>
+                    <FaPlusCircle
+                      className="text-success fs-1 ms-5"
+                      onClick={() => addToCart(item.id)}
+                    />
+                  </div>
+                )}
               </div>
             </div>
           </div>
@@ -29,4 +47,4 @@ function Product() {
   );
 }
 
-export default Product;
+export default item;
--- src/components/context/ProductProvider.jsx
+++ src/components/context/ProductProvider.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProductContext } from "./ProductContext";
 import { productList } from "../../assets/assets";
 
 function ProductProvider({ children }) {
+  const [cartItems, setCartItems] = useState({});
+  const addToCart = (item_id) => {
+    if (!cartItems[item_id]) {
+      setCartItems((prev) => ({ ...prev, [item_id]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [item_id]: prev[item_id] + 1 }));
+    }
+  };
+
+  const removeFromCart = (item_id) => {
+    setCartItems((prev) => ({ ...prev, [item_id]: prev[item_id] - 1 }));
+  };
+
+  const ContextValue = {
+    productList,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+  };
   return (
-    <ProductContext.Provider value={productList}>
+    <ProductContext.Provider value={ContextValue}>
       {children}
     </ProductContext.Provider>
   );
